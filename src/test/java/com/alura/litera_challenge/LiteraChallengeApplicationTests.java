7e672f1fa01a29762038ef69f9daeea16e3c5e1c
JBDolmus <jbautista.dormo.corea@gmail.com>
2025-05-30 16:28:29 -0600

    init repository challenge

---
+++ src/test/java/com/alura/litera_challenge/LiteraChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.litera_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
